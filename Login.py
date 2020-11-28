24ba36a9f51005c1e578ace4067b940e59c7c1f1
chedandan <243304341@qq.com>
2020-11-28 19:31:45 +0800

    a

--- Login.py
+++ Login.py
@@ -1,11 +1,11 @@
 import requests
 import os
 
-header1 = {
+header_get = {
     'X-Unity-Version': '2018.4.7f1',
     'GCA': 'X',
     'Content-Type': 'application/x-www-form-urlencoded',
-    'CDNDataVersion': '6262',
+    'CDNDataVersion': '6310',
     'User-Agent': 'HousamoAPI/4.10.2 Android OS 6.0.1 / API-23 (V417IR/eng.luoweiqiao.20201016.150344) Netease MuMu',
     'Response-Crypt': 'enable',
     'Host': 'elb.housamo.jp',
@@ -13,12 +13,12 @@ header1 = {
     'Accept-Encoding': 'gzip',
 }
 
-header2 = {
+header_post = {
     'Expect': '100-continue',
     'X-Unity-Version': '2018.4.7f1',
     'GCA': 'X',
     'Content-Type': 'application/x-www-form-urlencoded',
-    'CDNDataVersion': '6262',
+    'CDNDataVersion': '6310',
     'User-Agent': 'HousamoAPI/4.10.2 Android OS 6.0.1 / API-23 (V417IR/eng.luoweiqiao.20201016.150344) Netease MuMu',
     'Response-Crypt': 'enable',
     'Content-Length': '57',
@@ -31,13 +31,13 @@ header2 = {
 def Login(auth_key):
     data = {'auth_key': auth_key}
 
-    url1 = 'http://elb.housamo.jp/user/status?auth_key=' + auth_key
-    url2 = 'http://elb.housamo.jp/account/login'
-    url3 = 'https://elb.housamo.jp/user/status?auth_key=' + auth_key
+    url1 = 'http://elb.housamo.jp/account/login'
+    url2 = 'https://elb.housamo.jp/user/status?auth_key=' + auth_key
+    url3 = 'http://elb.housamo.jp/mypage/status?auth_key=' + auth_key
 
-    res1 = requests.get(url1, headers=header1)
-    res2 = requests.post(url2, headers=header2, data=data)
-    res3 = requests.get(url3, headers=header1)
+    res1 = requests.post(url1, headers = header_post, data = data)
+    res2 = requests.get(url2, headers = header_get)
+    res3 = requests.get(url3, headers = header_get)
 
     print(res1.content, '\n')
     print(res2.content, '\n')
