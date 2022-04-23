4692695f5042f64a6a8d23c435f4d4d01e0c89bf
dandan <243304341@qq.com>
2022-04-23 16:17:51 +0800

    Update Login.py

--- Login.py
+++ Login.py
@@ -50,11 +50,12 @@ def login_by_authkey(auth_key):
 
 if __name__ == '__main__':
     auth_keys = os.environ['auth_keys']
+    print(auth_keys)
 #     pattern = re.compile(r'AUTH_KEY_.*')                #匹配环境变量中的auth_key
 #     for item in auth_keys.keys():
 #         if pattern.match(item):
 #             auth_key = auth[item]
 #             login_by_authkey(auth_key) 
-    for auth_key in auth_keys.values():
-        login_by_authkey(auth_key) 
+#     for auth_key in auth_keys.values():
+#         login_by_authkey(auth_key) 
 
